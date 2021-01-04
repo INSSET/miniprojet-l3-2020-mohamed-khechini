90033050ad605466c63c18521e1a04512c718622
KHECHINI MED <khechinimed@KHECHINIs-MacBook-Pro.local>
2021-01-04 18:22:05 +0100

    affichage des posts de la base de données

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
@@ -13,8 +15,11 @@ class BlogController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        $posts = $this->getDoctrine()->getRepository(Post::class)
+                        ->findBy([], ['time'=>'DESC']);
 
+        return $this->render('blog/index.html.twig', [
+            'posts' => $posts
         ]);
     }
 }
