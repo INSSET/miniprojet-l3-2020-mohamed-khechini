be9f27eb169995970be6e3f6d2ff10332ae2d5a9
KHECHINI MED <khechinimed@KHECHINIs-MacBook-Pro.local>
2021-01-07 19:43:30 +0100

    données Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,61 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Post;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Cocur\Slugify\Slugify;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    public function load(ObjectManager $manager){
+        $this->loadUsers($manager);
+        $this->loadPosts($manager);
+    }
+    public function loadPosts(ObjectManager $manager)
+    {
+        //Utilisation d'un generateur de slug pour remplacer les id
+        $slugify = new Slugify();
+        //Utilisation des fixtures pour des fausses donées
+        for($i=0;$i<15;$i++){
+            $post = new Post();
+            $post->setTitle("Titre n°".rand(0,100));
+            $post->setBody("Voici body Lorem ipsum bla blan°".rand(0,100));
+            $post->setTime(new \DateTime());
+            $post->setUser($this->getReference('khechini'));
+            $post->setSlug($slugify->slugify($post->getTitle()));
+
+            $manager->persist($post);
+        }
+        
+
+        $manager->flush();
+    }
+
+    public function loadUsers(ObjectManager $manager){
+            $user = new User();
+            $user->setUsername("khechinimed");
+            $user->setFullname("Khechini Mohamed");
+            $user->setEmail("khechinibakr20@gmail.com");
+            $user->setPassword(
+                $this->passwordEncoder->encodePassword(
+                    $user, 'baker_780'
+                )
+            );
+            $this->addReference('khechini', $user);
+            $manager->persist($user);
+        
+        
 
         $manager->flush();
     }
--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -10,20 +10,6 @@ class PostFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        //Utilisation d'un generateur de slug pour remplacer les id
-        $slugify = new Slugify();
-        //Utilisation des fixtures pour des fausses donées
-        for($i=0;$i<15;$i++){
-            $post = new Post();
-            $post->setTitle("Titre n°".rand(0,100));
-            $post->setBody("Voici body Lorem ipsum bla blan°".rand(0,100));
-            $post->setTime(new \DateTime());
-            $post->setSlug($slugify->slugify($post->getTitle()));
-
-            $manager->persist($post);
-        }
         
-
-        $manager->flush();
     }
 }
